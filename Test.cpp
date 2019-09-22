5b822814ba38612498c3e3d811fe063b98700740
vivekrauniyar <54347022+vivekrauniyar@users.noreply.github.com>
2019-09-22 23:41:01 +0530

    Delete Test.cpp

--- Test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main(int argc, char **argv) {
-    
-    // Finally we output the parameters and prices
-    std::cout << "I love you Aditi      " << std::endl;
-    
-    return 0;
-}
